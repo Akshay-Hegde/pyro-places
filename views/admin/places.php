55f530d3c2127a868870cf8a7a434e521eff3417
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2012-07-26 21:38:05 +0200

    Another bad tag day.

--- views/admin/places.php
+++ views/admin/places.php
@@ -1,9 +1,9 @@
 <section class="title">
-	<h4><?phpecho lang('places:places'); ?></h4>
+	<h4><?php echo lang('places:places'); ?></h4>
 </section>
 
 <section class="item">
-	<?phpecho form_open('admin/places/delete');?>
+	<?php echo form_open('admin/places/delete');?>
 	
 	<?phpif (!empty($places)): ?>
 	
@@ -11,10 +11,10 @@
 			<thead>
 			    <!-- Header row -->
 				<tr>
-					<th><?phpecho form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
-                    <th><?phpecho lang('places:id'); ?></th>
-					<th><?phpecho lang('places:name'); ?></th>
-					<th><?phpecho lang('places:address'); ?></th>
+					<th><?php echo form_checkbox(array('name' => 'action_to_all', 'class' => 'check-all'));?></th>
+                    <th><?php echo lang('places:id'); ?></th>
+					<th><?php echo lang('places:name'); ?></th>
+					<th><?php echo lang('places:address'); ?></th>
 					<th></th>
 				</tr>
 			</thead>
@@ -30,10 +30,10 @@
 				    foreach( $places as $location ):
 			    ?>
 				<tr>
-					<td><?phpecho form_checkbox('action_to[]', $location->id); ?></td>
-					<td><?phpecho $location->id; ?></td>
-					<td><?phpecho $location->name; ?></td>
-					<td><?phpecho $location->address; ?></td>
+					<td><?php echo form_checkbox('action_to[]', $location->id); ?></td>
+					<td><?php echo $location->id; ?></td>
+					<td><?php echo $location->name; ?></td>
+					<td><?php echo $location->address; ?></td>
 					<td class="actions">
 						<?php echo
 						anchor('http://maps.googleapis.com/maps/api/staticmap?'.$default_params.'&
@@ -53,8 +53,8 @@
 		</div>
 		
 	<?phpelse: ?>
-		<div class="no_data"><?phpecho lang('sports:no_items'); ?></div>
+		<div class="no_data"><?php echo lang('sports:no_items'); ?></div>
 	<?phpendif;?>
 	
-	<?phpecho form_close(); ?>
+	<?php echo form_close(); ?>
 </section>
